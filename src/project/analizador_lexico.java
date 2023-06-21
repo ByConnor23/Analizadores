8bb1ffa88f523b2b59b533834163c7aad284c4fa
WicasZ <williamhumbertozi@hotmail.com>
2023-06-21 01:47:25 -0600

    Cambio analizador_lex

--- src/project/analizador_lexico.java
+++ src/project/analizador_lexico.java
@@ -2,88 +2,127 @@ package project;
 
 import java.io.*;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class analizador_lexico {
 
-    public static void main(String[] args) {
-        String sourceCodeFile = "fuente.txt"; // Ruta del archivo fuente
-        String symbolTableFile = "tabla_simbolos.txt"; // Ruta del archivo de la tabla de símbolos
-        
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(sourceCodeFile));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(symbolTableFile));
+    //Constructor este al ser llamado toma como parametro una cadena que debera provenir de la ventana 
+    public analizador_lexico(String sourceCode) {
+        //Genero los tokens y clasifico
+        List<String> tokens = tokenize(sourceCode);
+        //Genero el archivo tabla de simbolos y escribo los tokens que anteriormente analice
+        generateTable(tokens);
+    }
 
-            String line;
-            int lineNumber = 1;
-            while ((line = reader.readLine()) != null) {
-                line = removeComments(line);
-                analyzeLine(line, writer, lineNumber);
-                lineNumber++;
+    private static void generateTable(List<String> tokens) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("tabla_simbolos.txt"))) {
+            for (String token : tokens) {
+                writer.write(token);
+                writer.newLine();
             }
-
-            reader.close();
-            writer.close();
-
-            System.out.println("Análisis léxico completado. Se ha generado el archivo de la tabla de símbolos.");
-        } catch (IOException e) {
+        } catch (Exception e) {
+            // TODO: handle exception
             System.err.println("Error al leer el archivo fuente: " + e.getMessage());
         }
     }
 
-    private static void analyzeLine(String line, BufferedWriter writer, int lineNumber) throws IOException {
-        // Expresiones regulares para identificar los diferentes tipos de tokens
-        String keywordRegex = "\\b(int|double|string|bool|false|true|loopFor|If|else|key|case|breaker|start|add|minus|multi|div|new|back|list|Screen|menu|go|define|color|Image|Sound|setBackground|playSound|stopSound|chBox|Character)\\b";
-        String identifierRegex = "[a-zA-Z][a-zA-Z0-9_]*";
-        String stringRegex = "\"[^\"]*\"";
-        String numberRegex = "\\d+(\\.\\d+)?";
-        String punctuationRegex = "[.,;(){}\\[\\]]";
+    private static List<String> tokenize(String sourceCode) {
+        List<String> tokens = new ArrayList<>();
 
-        // Dividir la línea en tokens
-        String[] tokens = line.split("\\s+");
+        Pattern stringPattern = Pattern.compile("(['\"])(.*?)\\1"); // busca cadenas
+        Pattern identifierPattern = Pattern.compile("[a-zA-Z_][a-zA-Z0-9_]*");// busca palabras
+        Pattern numberPattern = Pattern.compile("\\d+");// busca numeros
+        Pattern punctuationPattern = Pattern.compile("[(){};,\\.=]");// busca signos de puntuacion
+        Pattern separadoPattern = Pattern.compile("[\n  ]");// busca separadores
 
-        int i = 0;
-        while (i < tokens.length) {
-            String token = tokens[i];
+        Matcher matcher;
 
-            if (token.matches(keywordRegex)) {
-                writer.write(token + "\t\t\tKeyword\n");
-            } else if (token.matches(identifierRegex)) {
-                writer.write(token + "\t\t\tIdentifier\n");
-            } else if (token.matches(stringRegex)) {
-                writer.write(token + "\t\t\tString\n");
-            } else if (token.matches(numberRegex)) {
-                writer.write(token + "\t\t\tNumber\n");
-            } else if (token.matches(punctuationRegex)) {
-                writer.write(token + "\t\t\tPunctuation\n");
-            } else {
-                // Combinar tokens hasta encontrar el cierre de comillas
-                if (token.startsWith("\"") && !token.endsWith("\"")) {
-                    StringBuilder combinedToken = new StringBuilder(token);
-                    i++;
-                    while (i < tokens.length && !tokens[i].endsWith("\"")) {
-                        combinedToken.append(" ").append(tokens[i]);
-                        i++;
-                    }
-                    if (i < tokens.length) {
-                        combinedToken.append(" ").append(tokens[i]);
-                        writer.write(combinedToken.toString() + "\t\t\tString\n");
-                    } else {
-                        writer.write(combinedToken.toString() + "\t\t\tOther\n");
-                    }
-                } else {
-                    writer.write(token + "\t\t\tOther\n");
-                }
+        while (!sourceCode.isEmpty()) {
+            // Busco coicidencias con los tokens
+            //Si encuentan alguna guarda dependiendo de cual coicidencia sea
+            matcher = stringPattern.matcher(sourceCode);
+            if (matcher.lookingAt()) {
+                String match = matcher.group();
+                tokens.add("String: " + match);
+                sourceCode = sourceCode.substring(match.length());
+                continue;
             }
 
-            i++;
+            matcher = identifierPattern.matcher(sourceCode);
+            if (matcher.lookingAt()) {
+                String match = matcher.group();
+                if (isKeyword(match)) {
+                    tokens.add("Keyword: " + match);
+                } else {
+                    tokens.add("Identifier: " + match);
+                }
+                sourceCode = sourceCode.substring(match.length());
+                continue;
+            }
+
+            matcher = numberPattern.matcher(sourceCode);
+            if (matcher.lookingAt()) {
+                String match = matcher.group();
+                tokens.add("Number: " + match);
+                sourceCode = sourceCode.substring(match.length());
+                continue;
+            }
+
+            matcher = punctuationPattern.matcher(sourceCode);
+            if (matcher.lookingAt()) {
+                String match = matcher.group();
+                tokens.add("Punctuation: " + match);
+                sourceCode = sourceCode.substring(match.length());
+                continue;
+            }
+
+            matcher = separadoPattern.matcher(sourceCode);
+            if (matcher.lookingAt()) {
+                String match = matcher.group();
+                sourceCode = sourceCode.substring(match.length());
+                continue;
+            }
+
+            // Si no se encuentra ningún patrón, se muestra "Token no reconocido" y se
+            // detiene la ejecución
+            tokens.add("Token no reconocido" + sourceCode);
+            break;
+            // sourceCode = sourceCode.substring(1);
         }
+
+        return tokens;
     }
 
-    private static String removeComments(String input){
-        //Eliminar comentarios de varias lineas
-        input = input.replaceAll("##.*?##", "");
-        //Eliminar comentarios de una linea
-        input = input.replaceAll("/#.*(?<!\\n)", "");
-        return input;
+    private static boolean isKeyword(String token) {
+        // Lista de palabras clave de ejemplo
+        String[] keywords = { "int", "double", "string", "bool", "false", "true", "loopFor", "If", "else", "key",
+                "case", "breaker", "start", "add", "minus", "multi", "div", "new", "back", "list", "Screen", "menu",
+                "go", "define", "color", "Image", "Sound", "setBackground", "playSound", "stopSound", "chBox",
+                "Character" }; //Mario y sus palabras claves
+        return Arrays.asList(keywords).contains(token);
+    }
+
+    // Prueba de uso
+    public static void main(String[] args) {
+        String sourceCode = "define p1 = Character (\'Marta\' , color = \"#92nd9s\");\n" +
+                "define p2 = Character ( \'Yo\' , color = \"#c8c8ff\" );\n" +
+                "Screen start ( ) {\n" +
+                "        ( \"Se lo eguntaré...\" );\n" +
+                "        p1 ( \"Um... te gustaría...\" );\n" +
+                "        p1 ( \"¿Te gustaría ir al cine después de la escuela?\" );\n" +
+                "       ( \"Silencio.\" );\n" +
+                "       ( \"El se sorprendió por tan repentina pregunta, y entonces...\" );" +
+                "       p2 ( \"Claro, me encantaría ir contigo\" );" +
+                "        ( \"El no sabía que esa noche su vida cambiaria para siempre\" );" +
+                "   end;\n" +
+                "}";
+
+        analizador_lexico lex = new analizador_lexico(sourceCode);
+        /*
+         * for (String token : tokens) {
+         * System.out.println(token);
+         * }
+         */
     }
 }
