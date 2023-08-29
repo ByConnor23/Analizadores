1b3fce2f3d46589d8c4a793e62e13ce1ec4b5336
Mario Balan Kantun <74071395+ByConnor23@users.noreply.github.com>
2023-08-28 23:33:53 -0600

    Object Token

--- src/project/Token.java
+++ src/project/Token.java
@@ -1,89 +1,50 @@
 package project;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Token {
+    private TokenType type;
+    private String value;
+    private int line;
+    private int column;
 
-    public Token() {
+    //Constructor
+    public Token(TokenType type, String value, int line, int column) {
+        this.type = type;
+        this.value = value;
+        this.line = line;
+        this.column = column;
     }
 
-    public Token(String type, String text) {
+    public TokenType getType() {
+        return type;
     }
 
-    public static List<String> tokenize(String sourceCode) {
-        List<String> tokens = new ArrayList<>();
-
-        Pattern stringPattern = Pattern.compile("(['\"])(.*?)\\1"); // busca cadenas
-        Pattern identifierPattern = Pattern.compile("[a-zA-Z_][a-zA-Z0-9_]*");// busca palabras
-        Pattern numberPattern = Pattern.compile("\\d+");// busca numeros
-        Pattern punctuationPattern = Pattern.compile("(==|<=|>=|<|>|\\+\\+|--|\\+|-|\\*|/|\\(|\\)|\\[|\\]|\\{|\\}|;|,|\\.|=|:|#)");// busca signos de puntuacion
-        Pattern separadoPattern = Pattern.compile("[\n\\s]");// busca separadores
-
-        Matcher matcher;
-
-        while (!sourceCode.isEmpty()) {
-            // Busco coincidencias con los tokens
-            // Si encuentran alguna, la guarda dependiendo de cuál coincidencia sea
-            matcher = stringPattern.matcher(sourceCode);
-            if (matcher.lookingAt()) {
-                String match = matcher.group();
-                tokens.add(match);
-                sourceCode = sourceCode.substring(match.length());
-                continue;
-            }
-
-            matcher = identifierPattern.matcher(sourceCode);
-            if (matcher.lookingAt()) {
-                String match = matcher.group();
-                if (isKeyword(match)) {
-                    tokens.add(match);
-                } else {
-                    tokens.add(match);
-                }
-                sourceCode = sourceCode.substring(match.length());
-                continue;
-            }
-
-            matcher = numberPattern.matcher(sourceCode);
-            if (matcher.lookingAt()) {
-                String match = matcher.group();
-                tokens.add(match);
-                sourceCode = sourceCode.substring(match.length());
-                continue;
-            }
-
-            matcher = punctuationPattern.matcher(sourceCode);
-            if (matcher.lookingAt()) {
-                String match = matcher.group();
-                tokens.add(match);
-                sourceCode = sourceCode.substring(match.length());
-                continue;
-            }
-
-            matcher = separadoPattern.matcher(sourceCode);
-            if (matcher.lookingAt()) {
-                String match = matcher.group();
-                sourceCode = sourceCode.substring(match.length());
-                continue;
-            }
-
-            // Si no se encuentra ningún patrón, se muestra "Token no reconocido" y se
-            // detiene la ejecución
-            tokens.add(sourceCode);
-            break;
-        }
-
-        return tokens;
+    public void setType(TokenType type) {
+        this.type = type;
     }
 
-    public static boolean isKeyword(String token) {
-        // Lista de palabras clave de ejemplo
-        String[] keywords = { "public", "static", "void", "main", "int", "double", "string", "bool", "false", "true", "if", "else",
-                "breaker", "Screen", "Menu", "go", "define", "Color", "Image", "Sound", "setBackground", "playSound", "stopSound", "Character" }; // Mario y sus palabras claves
-        return Arrays.asList(keywords).contains(token);
+    public String getValue() {
+        return value;
     }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public void setLine(int line) {
+        this.line = line;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    
 }
